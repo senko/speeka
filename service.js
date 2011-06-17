7b58ef3701c6c43b0a7bd42f61c5e6c6343d12c4
Senko Rasic <senko.rasic@dobarkod.hr>
2011-06-18 01:22:12 +0200

    add room backlog for 100 last events

--- service.js
+++ service.js
@@ -15,12 +15,47 @@ server = http.createServer(function (req, res) {
 var everyone = nowjs.initialize(server);
 var chatrooms = {};
 
+var BACKLOG_SIZE = 100;
+
+function backlog_append(room, clientId, nick, type, data) {
+    data = data || {};
+    data.type = type;
+    data.clientId = clientId;
+    data.nick = nick;
+    room.backlog.push(data);
+    console.log("Adding to backlog: " + data.type);
+    room.backlog = room.backlog.slice(-1 * BACKLOG_SIZE);
+}
+
+function backlog_replay(client, room) {
+    for (var i in room.backlog) {
+        var d = room.backlog[i];
+        switch (d.type) {
+            case 'join':
+                client.now.receiveJoin(d.clientId, d.nick);
+                break;
+            case 'leave':
+                client.now.receiveLeave(d.clientId, d.nick);
+                break;
+            case 'nick':
+                client.now.receiveNickChange(d.clientId, d.old_nick, d.nick);
+                break;
+            case 'message':
+                client.now.receiveMessage(d.clientId, d.nick, d.message);
+                break;
+            default:
+                console.log('Unknown event ' + d.type + ', ignoring');
+        }
+    }
+}
+
 everyone.now.createRoom = function(cb) {
     var room_id = Buffer(uuid.generate('binary')).toString('base64').slice(0, 10);
     var group = nowjs.getGroup(room_id);
 
     chatrooms[room_id] = {
         group: group,
+        backlog: [],
         members: {}
     };
 
@@ -43,7 +78,10 @@ everyone.now.joinRoom = function(room_id, nick, cb) {
     cb(true);
     r.group.addUser(this.user.clientId);
     r.members[this.user.clientId] = nick;
+
+    backlog_replay(this, r);
     r.group.now.receiveJoin(this.user.clientId, nick);
+    backlog_append(r, this.user.clientId, nick, 'join');
     console.log('Client ' + this.user.clientId + ' joined the room ' + room_id);
 
     this.now.receiveNicknameList(people);
@@ -59,7 +97,9 @@ everyone.now.sendMessage = function(room_id, message) {
     if (!message) return;
 
     r.group.now.receiveMessage(this.user.clientId, nick, message);
+
     console.log('Client ' + this.user.clientId + ' sent message in room ' + room_id);
+    backlog_append(r, this.user.clientId, nick, 'message', { message: message });
 }
 
 everyone.now.changeNickname = function(room_id, nick) {
@@ -73,6 +113,7 @@ everyone.now.changeNickname = function(room_id, nick) {
     r.group.now.receiveNickChange(this.user.clientId, old_nick, nick);
 
     console.log('Client ' + this.user.clientId + ' changed nickname');
+    backlog_append(r, this.user.clientId, nick, 'nick', { old_nick: old_nick });
 }
 
 everyone.on('disconnect', function() {
@@ -88,6 +129,7 @@ everyone.on('disconnect', function() {
 
         if (Object.keys(r.members).length > 0) {
             r.group.now.receiveLeave(this.user.clientId, nick);
+            backlog_append(r, this.user.clientId, nick, 'leave');
         } else{
             delete chatrooms[room_id];
             console.log('Deleted empty room ' + room_id);
